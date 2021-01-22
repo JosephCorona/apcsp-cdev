b502a7006626df2916d0b9776e01b4785732ebbd
Joseph Corona <coronaj8135@my.sduhsd.net>
2021-01-22 09:01:41 +0000

    Added encrypt.c

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -10,7 +10,6 @@
 char CHARS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 int CHARS_LEN = 62;
 
-
 char shiftChar(char c, int shift, int direction)
 {
   // implement the character shift here:
@@ -22,6 +21,34 @@ char shiftChar(char c, int shift, int direction)
   //   shiftChar('c', 3, 1) : 'f'
   //   shiftChar('S', 2, 0) : 'P'
   //   shiftChar('b', 3, 0) : '8'
+for (int i = 0; i < CHARS_LEN; i++)
+{
+ if(CHARS[i] == c)
+  {
+   int index = i;
+   shift = shift % CHARS_LEN;
+   if(direction == 1)
+   {
+    for(int i = 0; i < shift; i++)
+    {
+     index++;
+     if(index == CHARS_LEN)
+      index = 0;
+     }
+  }
+   else
+   {
+    for(int i = 0; i < shift; i++)
+    {
+     index--;
+     if(index < 0)
+     index = CHARS_LEN - 1;
+    }
+   }
+    return CHARS[index];
+   }
+ }
+return c;
 }
 
 
@@ -59,5 +86,3 @@ void caesarDecrypt(char str[], int shift)
   int shifts[] = { shift };
   decrypt(str, shifts, 1);
 }
-
-
